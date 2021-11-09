ae809ba6e3cb44b57bd7ab8a12bc02ebdbc2c01f
e1b19059 <e1b19059@oit.ac.jp>
2021-11-09 15:11:54 +0900

    リポジトリの初期化

---
+++ src/main/java/team3/gomoku/GomokuApplication.java
@@ -0,0 +1,13 @@
+package team3.gomoku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GomokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GomokuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team3/gomoku/GomokuApplicationTests.java
@@ -0,0 +1,13 @@
+package team3.gomoku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GomokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
