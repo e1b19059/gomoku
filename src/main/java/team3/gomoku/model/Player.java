769101492799e6e15d13f61d4224088e634d1a56
e1b19059 <e1b19059@oit.ac.jp>
2021-11-23 14:10:40 +0900

    マージ

---
+++ src/main/java/team3/gomoku/model/Player.java
@@ -0,0 +1,22 @@
+package team3.gomoku.model;
+
+public class Player {
+  int id;
+  String name;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
